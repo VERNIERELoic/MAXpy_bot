2aa4e55e9d1396292f2299dd855ec18e2ced8a74
VERNIERELoic <loic.verniere@cpe.fr>
2022-06-26 23:03:56 +0200

    objs version

--- src/main.py
+++ src/main.py
@@ -1,18 +1,46 @@
-from aquisition import *
-from  analysis import *
+from travel import *
+from configparser import ConfigParser
 import time
+import json
+import requests
+import os
+
+def get_config():
+    
+    list_config = []
+    list_value = []
+    config = ConfigParser()
+    config.read("config.ini")
+
+    for each_section in config.sections():
+        tab = []
+        for (each_key, each_val) in config.items(each_section):
+              tab.append(config[each_section][each_key])
+        list_config.append(tab)
+
+    return list_config
+
+get_config()
+
+
 
 def main():
-    
-    url = request_custo()
-    seats = get_seats(0,1)
 
+    list_config = get_config()
+    travels_objs = []
+    for i in range(len(list_config)):
+        travels_objs.append(Travel(list_config[i][0], list_config[i][1], list_config[i][2], ""+list_config[i][1]+"-->"+list_config[i][2]+""))
+
+    isFistTimeInLoop = True
     while(1):
-        if download_json(url):
-            seats = get_seats(seats)
-            time.sleep(5)
-        else:
-            time.sleep(5)
+        for i in range(len(travels_objs)):
+            travels_objs[i].set_url()
+            if travels_objs[i].get_data(i):
+                travels_objs[i].read_data(i, isFistTimeInLoop)
+                time.sleep(5)
+            else:
+                time.sleep(5)
+        isFistTimeInLoop = False
 
 
 if __name__ == "__main__":
---
+++ src/travel.py
@@ -0,0 +1,90 @@
+import json 
+import telegram
+import requests
+
+class Travel:
+    
+    def __init__(self, date, departure, arrival, travel):
+        self.url = ""
+        self.date = date
+        self.travel_config = travel
+        self.travel_json = ""
+        self.departure = departure
+        self.arrival = arrival
+        self.seats = 1
+        self.list_departures = []
+
+    def set_url(self):
+        self.date.replace("/", "%2F")
+
+        url = "https://data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&lang=FR&rows=1000&facet=date&facet=origine&facet=destination&facet=od_happy_card&refine.origine={}+(intramuros)&refine.destination={}+(gares+intramuros)&refine.date={}&refine.od_happy_card=OUI&timezone=Europe%2FParis".format(
+        self.departure, self.arrival, self.date)
+
+        self.url = url
+    
+    def set_seat(self, seats):
+        self.seats = seats
+
+    def set_travel(self,travel):
+        self.travel = travel
+
+    def read_data(self, i, isFistTimeInLoop):
+        with open('data/travel{}.json'.format(i)) as f:
+            data = json.load(f)
+        
+        new_seats = data['nhits']
+        
+        if new_seats != 0:
+
+            start = data['records'][0]['fields']['origine']
+            end = data['records'][0]['fields']['destination']
+
+            self.set_travel("{} --> {}".format(start, end))
+            seats_available = "Places TGVMAX disponibles : {}\n 📅 Date: {}\n 📍 Trajet: {}".format(self.seats, self.date, self.travel_json)
+
+            train_list = []
+
+            for train in data['records']:
+
+                train_id = train['fields']['train_no']
+                time = train['fields']['heure_depart']
+        
+                train_list.append(time)
+
+                messanger_train_data = "Depart : {}\n Numero de train : {}\n".format(
+                    time, train_id)
+
+            if self.seats != new_seats:
+                self.send_alert(""+seats_available+ "\n 🕑 "+ str(train_list) +"")
+                self.set_seat(new_seats)
+        else:
+            if isFistTimeInLoop == True:
+                no_train_data = "Dsl, Auncun train pour le moment pour :\n📅 Date: {}\n 📍 Trajet: {} \n Nous vous informerons des qu'une place est disponible !".format(self.date, self.travel_config)
+                self.send_alert(no_train_data)
+
+
+        self.seats = new_seats
+            
+                
+    def send_alert(self, message):
+        with open('data/keys.json', 'r') as keys_file:
+            k = json.load(keys_file)
+            token = k['telegram_token']
+            chat_id = k['telegram_chat_id']
+        bot = telegram.Bot(token=token)
+        bot.sendMessage(chat_id=chat_id, text=message)
+
+    def get_data(self, i):
+        request = requests.get(self.url).text
+        json_data = json.loads(request)
+
+        with open('data/travel{}.json'.format(i), 'w') as f:
+            json.dump(json_data, f)
+
+        if json_data is None:
+            print("Update failed !")
+            return False
+        else:
+            print("Update completed !")
+            return True
+
