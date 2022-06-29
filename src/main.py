ce2fdfa87fbdab642d62a50739de89a4a70e0953
VERNIERELoic <loic.verniere@cpe.fr>
2022-06-29 21:26:02 +0200

    fix

--- src/main.py
+++ src/main.py
@@ -5,8 +5,9 @@ import json
 import requests
 import os
 
+
 def get_config():
-    
+
     list_config = []
     list_value = []
     config = ConfigParser()
@@ -15,13 +16,10 @@ def get_config():
     for each_section in config.sections():
         tab = []
         for (each_key, each_val) in config.items(each_section):
-              tab.append(config[each_section][each_key])
+            tab.append(config[each_section][each_key])
         list_config.append(tab)
 
     return list_config
-
-get_config()
-
 
 
 def main():
@@ -29,15 +27,21 @@ def main():
     list_config = get_config()
     travels_objs = []
     for i in range(len(list_config)):
-        travels_objs.append(Travel(list_config[i][0], list_config[i][1], list_config[i][2], ""+list_config[i][1]+"-->"+list_config[i][2]+""))
+        travels_objs.append(
+            Travel(
+                list_config[i][0],
+                list_config[i][1],
+                list_config[i][2],
+                "" + list_config[i][1] + "-->" + list_config[i][2] + "",
+            )
+        )
 
     isFistTimeInLoop = True
     test = 1
-    while(1):
+    while 1:
         for i in range(len(travels_objs)):
             travels_objs[i].set_url()
-            #if travels_objs[i].get_data(i):
-            if test == 1:
+            if travels_objs[i].get_data(i):
                 travels_objs[i].read_data(i, isFistTimeInLoop)
                 time.sleep(5)
             else:
